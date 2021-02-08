37b8bc3b2c25347f2de132b87c1f9442fe4d4c2b
Satyam Naidu Bojanki <38328330+satyamgit5072@users.noreply.github.com>
2021-02-08 18:36:06 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 	public static void main(String[] args)
             {
 		System.out.println("Hello, World Check");
-		System.out.println("Hello world 2");
+		System.out.println("Hello world 1");
         }
 }
